c2c80c796428dc0f613e1079909d1645c23995ad
Ben Brandvig <benjamin.brandvig@libertymutual.com>
2017-08-23 13:03:29 -0700

    Realized after today's class that my use of a static variable was unnecessary

--- src/main/java/com/libertymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/libertymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -1,8 +1,8 @@
 package com.libertymutual.goforcode.romanNumerals.services;
 
 public class HinduToRomanNumeralConverter {
-	private static int[] denominations = new int[] {5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-	private static String[] numerals = new String[] {"?", "M?", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	private int[] denominations = new int[] {5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	private String[] numerals = new String[] {"?", "M?", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
 	public String convert(int input)	{	
 		int remainingInput = input;
